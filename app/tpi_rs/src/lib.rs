44012d4cd9ed1ba36367e00a898213ff23dafb07
Ruslan Akbashev <ra@turingpi.com>
2023-06-23 05:32:47 +0400

    move tpi_rs to a separate repository
    The code now lives in github.com/turing-machines/tpi_rs
    
    Also, modify TPI_RS_BUILD_CMDS to be closer to original, since some
    hacks (like removing --offline, --locked flags) are no longer needed.

--- app/tpi_rs/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod app;
-pub mod c_interface;
-mod middleware;
--- tp2bmc/package/tpi_rs/tpi_rs.mk
+++ tp2bmc/package/tpi_rs/tpi_rs.mk
@@ -4,29 +4,30 @@
 #
 ###########################################################
 
-TPI_RS_VERSION = 0.0.0
-TPI_RS_SITE = $(TOPDIR)/../app/tpi_rs
-TPI_RS_SITE_METHOD = local
+TPI_RS_VERSION = 1.1.0
+TPI_RS_SITE = $(call github,turing-machines,tpi_rs,v$(TPI_RS_VERSION))
 TPI_RS_LICENSE = Apache-2.0
 TPI_RS_LICENSE_FILES = LICENSE
 TPI_RS_INSTALL_STAGING = YES
 TPI_RS_INSTALL_TARGET = NO
-TPI_RS_CARGO_ENV = PKG_CONFIG_ALLOW_CROSS=1
+TPI_RS_CARGO_ENV := PKG_CONFIG_ALLOW_CROSS=1 # for `libusb1-sys`
+TPI_RS_CARGO_ENV += CC_armv7_unknown_linux_gnueabi="arm-linux-gcc" # for `ring`
 TPI_RS_DEPENDENCIES = libusb
 
-# This build step is derived from the default build cmd. Removed the TARGET_CONFIGURE_OPTS variable
-# as some of the compiler flags where causing cross compilation to fail. Secondly, Removed the
-# --offline requirement, having this package referenced locally causes some issues downloading all
-# cargo dependencies as well
+# This build step is the default build command (see pkg-cargo.mk), but with TARGET_CONFIGURE_OPTS
+# variable removed, as providing environment variable CC="…/arm-buildroot-linux-gnueabi-gcc" causes
+# compilation to fail.
 define TPI_RS_BUILD_CMDS
 	cd $(TPI_RS_SRCDIR) && \
 	$(TARGET_MAKE_ENV) \
 		$(PKG_CARGO_ENV) \
 		$(TPI_RS_CARGO_ENV) \
-		env CC_armv7-unknown-linux-gnueabi="arm-linux-gcc" \
 		cargo build \
+			--offline \
 			$(if $(BR2_ENABLE_DEBUG),,--release) \
-			--manifest-path Cargo.toml
+			--manifest-path Cargo.toml \
+			--locked \
+			$(TPI_RS_CARGO_BUILD_OPTS)
 endef
 
 define TPI_RS_INSTALL_STAGING_CMDS
