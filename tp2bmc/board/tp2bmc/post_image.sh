53f2c0fc9490557b20450c72e526d39cfce9a25a
Sam Edwards <CFSworks@gmail.com>
2023-07-25 18:15:52 -0600

    installer: build and package the microSD installer program

--- tp2bmc/board/tp2bmc/post_image.sh
+++ tp2bmc/board/tp2bmc/post_image.sh
@@ -4,11 +4,18 @@ set -euo pipefail
 BOARD_DIR="${0%/*}"
 cd $BINARIES_DIR
 
+# Prepare the installer initramfs image
+INITRAMFS_DIR=$STAGING_DIR/initramfs/install
+(cd $INITRAMFS_DIR && find .) |\
+	cpio -oH newc -D $INITRAMFS_DIR |\
+	gzip > installer.cpio.gz
+
 # Prepare the SD image's FAT32 filesystem
 mkdir -p sdcard-bootpart/boot
 FDT_FILENAME=$(basename $TARGET_DIR/boot/*.dtb)
 sed s/FDT_FILENAME_HERE/${FDT_FILENAME}/g < $BOARD_DIR/install.scr > /tmp/install.scr
 mkimage -A arm -T script -d /tmp/install.scr sdcard-bootpart/boot/boot.scr.uimg
+mkimage -A arm -T ramdisk -d installer.cpio.gz sdcard-bootpart/boot/install.img
 cp -r $BOARD_DIR/sdcard_overlay/* sdcard-bootpart/
 
 # Generate the SD image
---
+++ tp2bmc/package/bmc_installer/bmc_installer.mk
@@ -0,0 +1,19 @@
+###########################################################
+#
+# bmc_installer
+#
+###########################################################
+
+BMC_INSTALLER_VERSION = 0.1.0
+BMC_INSTALLER_SITE = $(call github,turing-machines,BMC-Installer,$(BMC_INSTALLER_VERSION))
+BMC_INSTALLER_LICENSE = Apache-2.0
+BMC_INSTALLER_LICENSE_FILES = LICENSE
+BMC_INSTALLER_INSTALL_STAGING = YES
+BMC_INSTALLER_INSTALL_TARGET = NO
+BMC_INSTALLER_CARGO_BUILD_OPTS = --bin=sdcard
+
+define BMC_INSTALLER_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/target/$(RUSTC_TARGET_NAME)/$(if $(BR2_ENABLE_DEBUG),debug,release)/sdcard $(STAGING_DIR)/initramfs/install/init
+endef
+
+$(eval $(cargo-package))
