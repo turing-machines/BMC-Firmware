4234f013f4451b223089c3516123ffbfee33def7
Sam Edwards <CFSworks@gmail.com>
2023-07-29 23:44:08 -0600

    general: nuke swupdate

--- tp2bmc/swupdate/genSWU.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-CONTAINER_VER="$1"
-PRODUCT_NAME="turingpi"
-FILES="sw-description rootfs.ubifs env0.fex env1.fex"
-for i in $FILES;do
-        echo $i;done | cpio -ov -H crc >  ${PRODUCT_NAME}_${CONTAINER_VER}.swu
