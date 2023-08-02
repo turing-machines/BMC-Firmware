e22fdac997b9cec68a95c9f20d93be749e77bd32
Sven Rademakers <sven.rademakers@gmail.com>
2023-08-02 13:54:44 +0100

    tpi: update tpi tool to new rust version

--- tp2bmc/package/tpi/tpi.mk
+++ tp2bmc/package/tpi/tpi.mk
@@ -1,17 +1,11 @@
 ###########################################################
 # turing pi CLI
 ###########################################################
-TPI_VERSION:= 1.0.0
-TPI_SITE:= $(TOPDIR)/../app/tpi    #这里是应用程序的位置。
-TPI_SITE_METHOD:=local
-TPI_INSTALL_TARGET:=YES
-define TPI_BUILD_CMDS
-$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
-endef
-define TPI_INSTALL_TARGET_CMDS
-$(INSTALL) -D -m 0755 $(@D)/tpi $(TARGET_DIR)/bin
-endef
-define TPI_PERMISSIONS
-/bin/tpi f 4755 0 0 - - - - -
-endef
-$(eval $(generic-package))
+TPI_VERSION:= 0.1.0
+TPI_SITE = $(call github,turing-machines,tpi,$(TPI_VERSION))
+TPI_INSTALL_TARGET =YES
+TPI_LICENSE = Apache-2.0
+TPI_LICENSE_FILES = LICENSE
+TPI_INSTALL_TARGET = YES
+
+$(eval $(cargo-package))
