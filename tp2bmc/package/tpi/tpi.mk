c5cf86e4b35fc1aa93674784af3a71494a681d61
Sven Rademakers <sven.rademakers@gmail.com>
2023-09-11 15:31:31 +0100

    bmcd: Add TLS
    Per default, the bmcd will generate new self signed certificates in the
    case they do not exist yet. (x509 rsa4096)
    
    HTTPS is enabled for both IPv4 and IPv6 on port 443.
    HTTP traffic will be redirected to the HTTPS port

--- tp2bmc/package/tpi/tpi.mk
+++ tp2bmc/package/tpi/tpi.mk
@@ -3,9 +3,7 @@
 ###########################################################
 TPI_VERSION:= 0.1.0
 TPI_SITE = $(call github,turing-machines,tpi,$(TPI_VERSION))
-TPI_INSTALL_TARGET =YES
 TPI_LICENSE = Apache-2.0
 TPI_LICENSE_FILES = LICENSE
-TPI_INSTALL_TARGET = YES
 
 $(eval $(cargo-package))
