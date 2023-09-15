c3e2dae4225c670266a70571309ba52dcf43aaf2
Sven Rademakers <sven.rademakers@gmail.com>
2023-09-15 14:13:15 +0100

    bump tpi & bmcd to latest unstable

--- tp2bmc/package/tpi/tpi.mk
+++ tp2bmc/package/tpi/tpi.mk
@@ -1,9 +1,10 @@
 ###########################################################
 # turing pi CLI
 ###########################################################
-TPI_VERSION:= 0.1.0
+TPI_VERSION:= 913d5fd3d134253042787d7a404f3a03e6b83772
 TPI_SITE = $(call github,turing-machines,tpi,$(TPI_VERSION))
 TPI_LICENSE = Apache-2.0
 TPI_LICENSE_FILES = LICENSE
+TPI_CARGO_INSTALL_OPTS = --features local-only
 
 $(eval $(cargo-package))
