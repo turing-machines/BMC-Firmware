9d0eff527e939da0c97122cc034a2013cef22fa7
Sven Rademakers <sven.rademakers@gmail.com>
2023-11-01 15:09:00 +0000

    v2.0.0-RC1
    Release candidate for v2.0.0. This version will be used for extensive
    testing and bug fixing.

--- tp2bmc/package/tpi/tpi.mk
+++ tp2bmc/package/tpi/tpi.mk
@@ -1,7 +1,7 @@
 ###########################################################
 # turing pi CLI
 ###########################################################
-TPI_VERSION:= d8c0bc6c1cd4f7054b8645b3398ba263662c8d0e
+TPI_VERSION:= v1.0.0
 TPI_SITE = $(call github,turing-machines,tpi,$(TPI_VERSION))
 TPI_LICENSE = Apache-2.0
 TPI_LICENSE_FILES = LICENSE
