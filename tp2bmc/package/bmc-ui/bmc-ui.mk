549863cb81ac3d9664e89b15b3759e400877b065
Sven Rademakers <sven.rademakers@gmail.com>
2023-11-03 15:38:44 +0000

    Move bmc-ui to its own repository

---
+++ tp2bmc/package/bmc-ui/bmc-ui.mk
@@ -0,0 +1,16 @@
+###########################################################
+#
+# bmc-ui
+###########################################################
+
+BMC_UI_VERSION = main
+BMC_UI_SITE = $(call github,turing-machines,BMC-UI,$(BMC_UI_VERSION))
+BMC_UI_LICENSE = GPL-2.0
+BMC_UI_LICENSE_FILES = LICENSE
+define BMC_UI_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/srv/bmcd/www/
+	cp -r $(BMC_UI_SRCDIR)/* $(TARGET_DIR)/srv/bmcd/www/
+	chmod 755 $(TARGET_DIR)/srv/bmcd/www/
+endef
+
+$(eval $(generic-package))
