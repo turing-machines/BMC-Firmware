1a33cb68b8bdc823f3c2eb18982a4dc419c97cfb
Sven Rademakers <sven.rademakers@gmail.com>
2023-11-15 17:57:45 +0000

    bmc-ui & bmcd fixes
    * reworked error handling
    * fixed issue with drop downs in responsive version
    * fixed labels of progress bars
    * fixed install os button not being greyed out during transfer
    
    * backup api now returns a streaming implementation of the tar.gzip
      compression
    * when the multipart upgrade gets dropped wait for the error message
      before sending response back

--- tp2bmc/package/bmc-ui/bmc-ui.mk
+++ tp2bmc/package/bmc-ui/bmc-ui.mk
@@ -3,7 +3,7 @@
 # bmc-ui
 ###########################################################
 
-BMC_UI_VERSION = v1.0.0
+BMC_UI_VERSION = 96cb38f35fa522edfea09f802ffe26daf839ce11
 BMC_UI_SITE = $(call github,turing-machines,BMC-UI,$(BMC_UI_VERSION))
 BMC_UI_LICENSE = GPL-2.0
 BMC_UI_LICENSE_FILES = LICENSE
