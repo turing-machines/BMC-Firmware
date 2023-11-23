467803d196496ca663a9e4a0f21985cd02efa001
Sven Rademakers <sven.rademakers@gmail.com>
2023-11-23 10:19:36 +0000

    BMC-UI: added boot pin & print UI version
    Toggling the boot pin was implied behavior when selecting a route as USB
    device in the old UI. In the new UI, setting a USB route is not toggling
    the boot pin anymore. In order to support old usecases the boot pin is
    put back on the UI in the form of a checkbox.
    
    The exact used BMC-UI version is printed in the UI itself

--- tp2bmc/package/bmc-ui/bmc-ui.mk
+++ tp2bmc/package/bmc-ui/bmc-ui.mk
@@ -3,7 +3,7 @@
 # bmc-ui
 ###########################################################
 
-BMC_UI_VERSION = v1.0.1
+BMC_UI_VERSION = v1.0.2
 BMC_UI_SITE = $(call github,turing-machines,BMC-UI,$(BMC_UI_VERSION))
 BMC_UI_LICENSE = GPL-2.0
 BMC_UI_LICENSE_FILES = LICENSE
@@ -11,6 +11,7 @@ define BMC_UI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/srv/bmcd/www/
 	cp -r $(BMC_UI_SRCDIR)/dist/* $(TARGET_DIR)/srv/bmcd/www/
 	chmod 755 $(TARGET_DIR)/srv/bmcd/www/
+	sed -i 's/%BMC_UI%/$(BMC_UI_VERSION)/g' $(TARGET_DIR)/srv/bmcd/www/index.html
 endef
 
 $(eval $(generic-package))
