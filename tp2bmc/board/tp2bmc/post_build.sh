1987577d584e7a094dc93eb2e6dc9a588d2e30a4
Sven Rademakers <sven.rademakers@gmail.com>
2023-12-13 17:08:57 +0000

    Enabled CDC-ACM on BMC_USB_OTG port

--- tp2bmc/board/tp2bmc/post_build.sh
+++ tp2bmc/board/tp2bmc/post_build.sh
@@ -6,3 +6,9 @@ cd "${0%/*}"
 FDT_FILENAME=$(basename $TARGET_DIR/boot/*.dtb)
 sed s/FDT_FILENAME_HERE/${FDT_FILENAME}/g < boot.scr > /tmp/boot.scr
 mkimage -A arm -T script -d /tmp/boot.scr $TARGET_DIR/boot/boot.scr.uimg
+
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+	grep -qE '^GS0::' ${TARGET_DIR}/etc/inittab || \
+    sed -i '/GENERIC_SERIAL/a\
+GS0::respawn:/sbin/getty -L ttyGS0 115200 vt100 # BMC-USB-OTG' ${TARGET_DIR}/etc/inittab
+fi
