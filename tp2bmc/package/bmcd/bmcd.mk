6032ae63504ea4c8684550d878ee7f79e3a83442
Sven Rademakers <sven.rademakers@gmail.com>
2024-03-19 09:44:32 +0000

    bmcd: fix loopback detection.
    Patched the rust toolchain to 1.75.0, as fix required function that's
    stable from 1.75.0.

--- tp2bmc/package/bmcd/bmcd.mk
+++ tp2bmc/package/bmcd/bmcd.mk
@@ -3,7 +3,7 @@
 # bmcd
 ###########################################################
 
-BMCD_VERSION = c5162080995736597e27f638c82718af902e5b00
+BMCD_VERSION = 8ec72c606a285a1c6fc002a60fb389d56a26e2bc
 BMCD_SITE = $(call github,turing-machines,bmcd,$(BMCD_VERSION))
 BMCD_LICENSE = Apache-2.0
 BMCD_LICENSE_FILES = LICENSE
