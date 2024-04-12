f0753187cf6a028cd2621d65be78439042e0ca0b
Sven Rademakers <sven.rademakers@gmail.com>
2024-04-12 09:53:41 +0100

    update bmcd & tpi packages to latest

--- tp2bmc/package/tpi/tpi.mk
+++ tp2bmc/package/tpi/tpi.mk
@@ -1,7 +1,7 @@
 ###########################################################
 # turing pi CLI
 ###########################################################
-TPI_VERSION:= 1e08bc82f6e635e91d2e370ca5db7beed5238ee4
+TPI_VERSION:= b6772ed0516db04b14bd7ec50511fb16eeb4a641
 TPI_SITE = $(call github,turing-machines,tpi,$(TPI_VERSION))
 TPI_LICENSE = Apache-2.0
 TPI_LICENSE_FILES = LICENSE
