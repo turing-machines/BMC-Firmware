70605091c773553c578f6932b8843cbb08e859a4
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-04-28 21:50:07 -0400

    feat: download bmc-ui from GitHub releases

--- tp2bmc/package/bmc-ui/bmc-ui.mk
+++ tp2bmc/package/bmc-ui/bmc-ui.mk
@@ -3,15 +3,14 @@
 # bmc-ui
 ###########################################################
 
-BMC_UI_VERSION = 18c45c3a62d2cf2d682e864be303adf13568fc7a
-BMC_UI_SITE = $(call github,turing-machines,BMC-UI,$(BMC_UI_VERSION))
+BMC_UI_VERSION = v2.1.0
+BMC_UI_SITE = https://github.com/turing-machines/BMC-UI/releases/download/$(BMC_UI_VERSION)
 BMC_UI_LICENSE = GPL-2.0
 BMC_UI_LICENSE_FILES = LICENSE
 define BMC_UI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/srv/bmcd/www/
-	cp -r $(BMC_UI_SRCDIR)/dist/* $(TARGET_DIR)/srv/bmcd/www/
+	cp -r $(BMC_UI_SRCDIR)* $(TARGET_DIR)/srv/bmcd/www/
 	chmod 755 $(TARGET_DIR)/srv/bmcd/www/
-	sed -i 's/%BMC_UI%/$(BMC_UI_VERSION)/g' $(TARGET_DIR)/srv/bmcd/www/index.html
 endef
 
 $(eval $(generic-package))
