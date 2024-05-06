f5cbae9e4c91adfa6988afddc1f10108a74898e2
Sebastian Barrenechea <sebastian@barrenechea.cl>
2024-05-06 11:29:05 -0400

    chore: target latest bmcd with SPA routes support

--- tp2bmc/package/bmcd/bmcd.mk
+++ tp2bmc/package/bmcd/bmcd.mk
@@ -3,7 +3,7 @@
 # bmcd
 ###########################################################
 
-BMCD_VERSION = 4e6dffa928629c1066f6c0cf4414d6c399b725b3
+BMCD_VERSION = a525f331627535fd7173f93554dba85fe641c42b
 BMCD_SITE = $(call github,turing-machines,bmcd,$(BMCD_VERSION))
 BMCD_LICENSE = Apache-2.0
 BMCD_LICENSE_FILES = LICENSE
