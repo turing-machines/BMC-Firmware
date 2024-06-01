4a929dbe40cf8444839991e0c7ae99634198e47d
Sven Rademakers <sven.rademakers@gmail.com>
2024-06-01 15:28:20 +0100

    eeprom: edit board_info layout

--- tp2bmc/package/tpi/tpi.mk
+++ tp2bmc/package/tpi/tpi.mk
@@ -1,7 +1,7 @@
 ###########################################################
 # turing pi CLI
 ###########################################################
-TPI_VERSION:= v1.0.6
+TPI_VERSION:= 6ef9484687f90a83b14660362decf76597f54fd6
 TPI_SITE = $(call github,turing-machines,tpi,$(TPI_VERSION))
 TPI_LICENSE = Apache-2.0
 TPI_LICENSE_FILES = LICENSE
