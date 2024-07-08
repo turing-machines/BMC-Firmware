de097391b63548acb8165bd9a39fb35d35036aa2
Sven Rademakers <sven.rademakers@gmail.com>
2024-07-08 10:40:13 +0100

    buildroot: update to 2024.05

--- setup_build.sh
+++ setup_build.sh
@@ -9,10 +9,11 @@
 #
 
 set -x
+BUILDROOT_VER="2024.05"
 
 download_dir=$(mktemp -d)
 install_dir="$1"
-buildroot_url='https://buildroot.org/downloads/buildroot-2024.02.tar.gz'
+buildroot_url="https://buildroot.org/downloads/buildroot-${BUILDROOT_VER}.tar.gz"
 buildroot=$(basename "$buildroot_url")
 buildroot_folder="${buildroot%.tar.gz}"
 project_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
