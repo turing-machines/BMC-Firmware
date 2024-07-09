e5f6158eeb61449a6b751e8febd27c6069cd56bb
Sven Rademakers <sven.rademakers@gmail.com>
2024-07-09 14:52:19 +0100

    factory: move video test
    * move operator question to the end of the process.
    * disable DSI conformation until further notice

--- tp2bmc/board/tp2bmc/factory_overlay/upper/factory/006-node1_peripheral_test.sh
+++ tp2bmc/board/tp2bmc/factory_overlay/upper/factory/006-node1_peripheral_test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 source node_helpers.sh
 
 # node 1 has a NVMe slot + MPCIe slot
 assert_pci_devices "1" "2"
-
-confirm "Do you see video output on the HDMI port?"
-confirm "Do you see video output on the DSI port?"
 
--- tp2bmc/board/tp2bmc/factory_overlay/upper/factory/012-key_test.sh
+++ tp2bmc/board/tp2bmc/factory_overlay/upper/factory/012-key_test.sh
@@ -2,5 +2,7 @@
 
 set -e
 source helpers.sh
+confirm "Do you see video on the HDMI monitor?"
+
 tpi power on > /dev/null
 confirm "Press 'Key1'. Do the red light go off?"
