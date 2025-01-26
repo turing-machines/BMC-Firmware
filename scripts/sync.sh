1898a10509e194078aff8d143292984b70e15102
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-01-25 23:05:41 -0500

    fix: ignore git directory during sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
     echo "Syncing repository to container"
-    rsync -hrPti --delete --update --fsync --exclude buildroot /mnt/ /work > /tmp/sync.log
+    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /mnt/ /work > /tmp/sync.log
 
     # Sync container back to repository
     echo "Syncing container back to repository"
-    rsync -hrPti --delete --update --fsync --exclude buildroot /work/ /mnt > /tmp/sync.log
+    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /work/ /mnt > /tmp/sync.log
 else
     echo "sync.sh is only used on macOS"
     exit 1
