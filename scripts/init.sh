d1b3654511d8af5ebeaf8f834b3ae010cb337868
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-01-25 20:23:46 -0500

    feat: redirect container sync to sync.sh for darwin

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,13 +8,8 @@ sudo chmod 777 /work /mnt
 sudo chown vscode:vscode /work
 
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
-    # Sync repository to container
-    echo "Syncing repository to container"
-    rsync -hvrPt --update --exclude buildroot /mnt/ /work 
-
-    # Sync container back to repository
-    echo "Syncing container back to repository"
-    rsync -hvrPt --update --exclude buildroot /work/ /mnt
+    # Sync container and host
+    "$(git rev-parse --show-toplevel)/scripts/sync.sh"
 fi
 
 echo "Container initialized"
