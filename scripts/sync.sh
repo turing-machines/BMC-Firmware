7297e5cbdce2b19a5421b4add462960b70225cd0
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-01-25 22:51:39 -0500

    fix: redirect macOS sync to logfile

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
     echo "Syncing repository to container"
-    rsync -hrPti --delete --update --fsync --exclude buildroot /mnt/ /work 
+    rsync -hrPti --delete --update --fsync --exclude buildroot /mnt/ /work > /tmp/sync.log
 
     # Sync container back to repository
     echo "Syncing container back to repository"
-    rsync -hrPti --delete --update --fsync --exclude buildroot /work/ /mnt
+    rsync -hrPti --delete --update --fsync --exclude buildroot /work/ /mnt > /tmp/sync.log
 else
     echo "sync.sh is only used on macOS"
     exit 1
