748b652bb7cbbdc7a945d910dcec28b18097fc49
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-01-28 11:33:08 -0500

    fix: initialize repo gitconfig before initial sync

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,6 +14,10 @@ echo "Enforcing permissions"
 sudo chmod 777 /work /mnt
 sudo chown vscode:vscode /work
 
+# Configure git config
+# initialize git config before initial sync of .git directory
+git config --local include.path ../.gitconfig
+
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
     # Sync Git only for initial sync
     if [[ ! -d "/work/.git" ]]; then
@@ -23,8 +27,5 @@ if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
     # Sync container and host
     "{root}/scripts/sync.sh"
 fi
-
-# Configure git config
-git config --local include.path ../.gitconfig
 
 echo "Container initialized"
