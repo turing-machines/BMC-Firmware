241e7dd39b71d9013b1d6ecee504212cb184fc2f
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-01-28 15:01:12 +0000

    fix: initial darwin sync

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,6 +8,11 @@ sudo chmod 777 /work /mnt
 sudo chown vscode:vscode /work
 
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
+    # Sync Git only for initial sync
+    if [[ ! -d "/work/.git" ]]; then
+        rsync -hrPti --delete --update --fsync --exclude buildroot /mnt/ /work > /tmp/sync.log
+    fi
+    
     # Sync container and host
     "$(git rev-parse --show-toplevel)/scripts/sync.sh"
 fi
