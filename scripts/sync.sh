10ba2582af568ec7289d29741edf758a209827ae
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-01-31 15:14:59 +0000

    fix: sync container to host before host to container

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,12 +4,12 @@
 set -eo pipefail
 
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
-    echo "Syncing repository to container"
-    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /mnt/ /work > /tmp/sync.log
-
     # Sync container back to repository
     echo "Syncing container back to repository"
     rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /work/ /mnt > /tmp/sync.log
+    
+    echo "Syncing repository to container"
+    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /mnt/ /work > /tmp/sync.log
 else
     echo "sync.sh is only used on macOS"
     exit 1
