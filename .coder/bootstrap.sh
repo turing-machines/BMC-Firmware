250ca06b7d0621ae1ad5852ef109415bce7d44ab
G.J.R. Timmer <gjr.timmer@gmail.com>
2025-02-04 16:28:45 +0100

    fix: git path

--- .coder/bootstrap.sh
+++ .coder/bootstrap.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 
-CLONE_PATH="${CLONE_PATH}"
-CLONE_PATH="$${CLONE_PATH/#\~/$${HOME}}"
+CLONE_PATH="${CLONE_PATH:-$(cd "$(dirname "${BASH_SOURCE[0]}")/../" && pwd)}"
 
 echo "Configuring Git"
-git -C "$${CLONE_PATH}" config include.path ../.gitconfig
+git -C "${CLONE_PATH}" config include.path ../.gitconfig
 
 echo "Configure Build Environment"
 sudo apt-get update
