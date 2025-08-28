d43c96223c9af4e33e1a1bf6fa5a0166e0862a99
Andreas Thienemann <andreas@bawue.net>
2025-08-28 13:37:17 +0200

    Fix up sync.sh script
    The `scripts/sync.sh` script will - when called as part of the `scripts/build.sh` stage - happily delete the just built
    firmware images as rsync will delete the `dist` folder.
    The script will also delete local changes the developer made in the buildroot, when running multiple times.
    
    To prevent that, do not execute --delete when syncing from container to host.
    Also exclude the dist folder when syncing from the host to the container.
    
    To make sync changes visible, use tee -a to show the log entries on the screen.

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,10 +6,10 @@ set -eo pipefail
 if [[ "${HOST_OS^^}" == "DARWIN" ]]; then
     # Sync container back to repository
     echo "Syncing container back to repository"
-    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /work/ /mnt > /tmp/sync.log
+    rsync -hrPti --update --fsync --exclude buildroot --exclude .git --exclude dist /work/ /mnt | tee -a /tmp/sync.log
     
     echo "Syncing repository to container"
-    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /mnt/ /work > /tmp/sync.log
+    rsync -hrPti --delete --update --fsync --exclude buildroot --exclude .git /mnt/ /work | tee -a /tmp/sync.log
 else
     echo "sync.sh is only used on macOS"
     exit 1
